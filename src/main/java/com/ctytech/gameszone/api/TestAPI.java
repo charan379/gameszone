4c8e9b04e3029774735f0a38356a5acda3171c83
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-03 17:33:40 +0530

    generalExceptionHandler added

--- src/main/java/com/ctytech/gameszone/api/TestAPI.java
+++ src/main/java/com/ctytech/gameszone/api/TestAPI.java
@@ -12,9 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class TestAPI {
 
     @GetMapping(value = "/t1/{id}")
-    public ResponseEntity<String> testOne(@PathVariable String id) {
+    public ResponseEntity<String> testOne(@PathVariable String id) throws Exception {
 
-        return new ResponseEntity<String>("Given string is : " + id, HttpStatus.OK);
+        throw new Exception("test error");
+        // return new ResponseEntity<String>("Given string is : " + id, HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/com/ctytech/gameszone/utility/ExceptionControllerAdvice.java
@@ -0,0 +1,29 @@
+package com.ctytech.gameszone.utility;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @Autowired
+    Environment environment;
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorInfo> generalExceptionHandler(Exception exception) {
+
+        ErrorInfo error = new ErrorInfo();
+
+        error.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        error.setErrorMessage(environment.getProperty("General.EXCEPTION_MESSAGE"));
+        error.setTimestamp(LocalDateTime.now());
+
+        return new ResponseEntity<ErrorInfo>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
