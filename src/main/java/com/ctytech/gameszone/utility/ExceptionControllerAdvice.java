9e4ce9742d63f2ffd4637a8de27b78c09b0d8241
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-03 18:09:53 +0530

    GameszoneExceptionHandler added

--- src/main/java/com/ctytech/gameszone/utility/ExceptionControllerAdvice.java
+++ src/main/java/com/ctytech/gameszone/utility/ExceptionControllerAdvice.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.ctytech.gameszone.exception.GameszoneException;
+
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
@@ -17,13 +19,25 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler
     public ResponseEntity<ErrorInfo> generalExceptionHandler(Exception exception) {
-
+        //
         ErrorInfo error = new ErrorInfo();
-
+        //
         error.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
         error.setErrorMessage(environment.getProperty("General.EXCEPTION_MESSAGE"));
         error.setTimestamp(LocalDateTime.now());
-
+        //
         return new ResponseEntity<ErrorInfo>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(GameszoneException.class)
+    public ResponseEntity<ErrorInfo> gameszoneExceptionHandler(GameszoneException exception) {
+        //
+        ErrorInfo error = new ErrorInfo();
+        //
+        error.setErrorCode(HttpStatus.BAD_REQUEST.value());
+        error.setErrorMessage(environment.getProperty(exception.getMessage()));
+        error.setTimestamp(LocalDateTime.now());
+        //
+        return new ResponseEntity<ErrorInfo>(error, HttpStatus.BAD_REQUEST);
     }
 }
