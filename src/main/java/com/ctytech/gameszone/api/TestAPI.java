1c2330c689995040390206183f0c2ac31be597a6
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-03 18:10:02 +0530

    test error

--- src/main/java/com/ctytech/gameszone/api/TestAPI.java
+++ src/main/java/com/ctytech/gameszone/api/TestAPI.java
@@ -2,7 +2,10 @@ package com.ctytech.gameszone.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
+
+import com.ctytech.gameszone.exception.GameszoneException;
+
+// import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +17,7 @@ public class TestAPI {
     @GetMapping(value = "/t1/{id}")
     public ResponseEntity<String> testOne(@PathVariable String id) throws Exception {
 
-        throw new Exception("test error");
+        throw new GameszoneException("Test.EXCEPTION_MESSAGE");
         // return new ResponseEntity<String>("Given string is : " + id, HttpStatus.OK);
     }
 
