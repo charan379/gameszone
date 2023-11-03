0f33ba60771385e6512c0f2ae983807d546e02b2
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-03 16:22:55 +0530

    initial commit

---
+++ src/main/java/com/ctytech/gameszone/GameszoneApplication.java
@@ -0,0 +1,13 @@
+package com.ctytech.gameszone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameszoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameszoneApplication.class, args);
+	}
+
+}
