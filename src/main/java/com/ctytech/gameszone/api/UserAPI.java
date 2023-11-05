7fae077f158756169ad2f7c6a7e42a01825e2e63
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-05 18:00:37 +0530

    getUser

--- src/main/java/com/ctytech/gameszone/api/UserAPI.java
+++ src/main/java/com/ctytech/gameszone/api/UserAPI.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ctytech.gameszone.dto.UserDTO;
@@ -12,7 +13,9 @@ import com.ctytech.gameszone.exception.GameszoneException;
 import com.ctytech.gameszone.service.UserService;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -31,4 +34,25 @@ public class UserAPI {
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
+    @GetMapping()
+    public ResponseEntity<UserDTO> getUser(
+            @RequestParam(required = false, name = "userId") Integer userId,
+            @RequestParam(required = false, name = "userName") String userName,
+            @RequestParam(required = false, name = "email") @Email(message = "{user.email.format.invalid}") String email)
+            throws GameszoneException {
+        //
+        UserDTO userDTO = new UserDTO();
+        if (userId != null) {
+            userDTO = userService.getUser(userId);
+        } else if (userName != null) {
+            userDTO = userService.getUser(userName);
+        } else if (email != null) {
+            userDTO = userService.getUserByEmail(email);
+        } else {
+            throw new GameszoneException("UserService.QUERY_NOT_PROVIDED");
+        }
+
+        userDTO.setPassword(null);
+        return new ResponseEntity<UserDTO>(userDTO, HttpStatus.OK);
+    }
 }
