cb460e6868b91058d405aed64799029953c93dfb
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-05 17:24:04 +0530

    findBy Email and userName

--- src/main/java/com/ctytech/gameszone/repository/UserRepository.java
+++ src/main/java/com/ctytech/gameszone/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.ctytech.gameszone.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.ctytech.gameszone.entity.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 
+    Optional<User> findByUserName(String userName);
 }
