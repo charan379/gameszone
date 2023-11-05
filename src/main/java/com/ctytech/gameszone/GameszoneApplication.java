6be8581e72fd14e6603fa156cae27460d80973bb
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-05 15:33:41 +0530

    added spring-boot-starter-security dependency

--- src/main/java/com/ctytech/gameszone/GameszoneApplication.java
+++ src/main/java/com/ctytech/gameszone/GameszoneApplication.java
@@ -2,14 +2,22 @@ package com.ctytech.gameszone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @PropertySource(value = { "classpath:messages.properties" })
 public class GameszoneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GameszoneApplication.class, args);
+	}
+
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
