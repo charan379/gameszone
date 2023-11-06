3f1119aac437313370d20be0e147e2a2e2fa9d51
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-06 17:12:10 +0530

    update game name API endpoint added

--- src/main/java/com/ctytech/gameszone/api/GameAPI.java
+++ src/main/java/com/ctytech/gameszone/api/GameAPI.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +20,9 @@ import com.ctytech.gameszone.service.GameService;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @RestController
 @Validated
@@ -64,5 +68,17 @@ public class GameAPI {
         Page<GameDTO> gamepage = gameService.searchGames(query, Integer.parseInt(pageNo), Integer.parseInt(limit));
 
         return new ResponseEntity<>(gamepage, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/update/{gameId}/name/{newGameName}")
+    public ResponseEntity<GameDTO> updateGameName(
+            @PathVariable(name = "gameId") @Pattern(regexp = "^[0-9]*$", message = "{game.gameId.invalid}") String gameId,
+            @PathVariable(name = "newGameName") @NotNull(message = "{game.gamename.absent}") @Size(max = 25, min = 3, message = "{game.gamename.invalid}") String newGameName)
+            throws GameszoneException {
+
+        //
+        GameDTO updatedGame = gameService.updateGameNameById(Integer.parseInt(gameId), newGameName);
+        //
+        return new ResponseEntity<GameDTO>(updatedGame, HttpStatus.OK);
     }
 }
