ff756f15916a00c995afd7086cef7260b4177b6a
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-09 11:10:04 +0530

    lazy fetch gameSlots

--- src/main/java/com/ctytech/gameszone/entity/Game.java
+++ src/main/java/com/ctytech/gameszone/entity/Game.java
@@ -9,6 +9,7 @@ import com.ctytech.gameszone.dto.SlotDTO;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,7 +30,7 @@ public class Game {
 
     private String image;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "gameId")
     private List<Slot> slots;
 
