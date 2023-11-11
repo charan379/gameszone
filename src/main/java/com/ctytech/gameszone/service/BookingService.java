0c85e55462597827910aa35121e34531e231a252
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-11 20:31:16 +0530

    fetch slot availabilty record

--- src/main/java/com/ctytech/gameszone/service/BookingService.java
+++ src/main/java/com/ctytech/gameszone/service/BookingService.java
@@ -1,9 +1,15 @@
 package com.ctytech.gameszone.service;
 
+import java.time.LocalDate;
+
 import com.ctytech.gameszone.dto.BookingDTO;
 import com.ctytech.gameszone.exception.GameszoneException;
+import com.ctytech.gameszone.record.SlotAvailabilityRecord;
 
 public interface BookingService {
 
     BookingDTO createNewBooking(BookingDTO bookingDTO) throws GameszoneException;
+
+    SlotAvailabilityRecord fetchSlotAvailability(Integer slotId, Integer gameId, LocalDate forDate)
+            throws GameszoneException;
 }
--- src/main/java/com/ctytech/gameszone/service/BookingServiceImpl.java
+++ src/main/java/com/ctytech/gameszone/service/BookingServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ctytech.gameszone.service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -8,11 +9,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ctytech.gameszone.dto.BookingDTO;
+import com.ctytech.gameszone.dto.SlotDTO;
 import com.ctytech.gameszone.entity.Booking;
 import com.ctytech.gameszone.entity.Game;
 import com.ctytech.gameszone.entity.Slot;
 import com.ctytech.gameszone.entity.User;
 import com.ctytech.gameszone.exception.GameszoneException;
+import com.ctytech.gameszone.record.SlotAvailabilityRecord;
 import com.ctytech.gameszone.repository.BookingRepository;
 import com.ctytech.gameszone.repository.GameRepository;
 import com.ctytech.gameszone.repository.SlotRepository;
@@ -38,12 +41,12 @@ public class BookingServiceImpl implements BookingService {
     public BookingDTO createNewBooking(BookingDTO bookingDTO) throws GameszoneException {
 
         // check if the slot of booking game is already booked on required day or not
-        Optional<Booking> alreadyBooked = bookingRepository
-                .findByBookingDateAndGameIdAndSlotId(bookingDTO.getBookingDate(), bookingDTO.getGameId(),
-                        bookingDTO.getSlotId());
+        // fetch slotAvailabilityRecord
+        SlotAvailabilityRecord slotAvailabilityRecord = fetchSlotAvailability(bookingDTO.getSlotId(),
+                bookingDTO.getGameId(), bookingDTO.getBookingDate());
 
         // if already booked throw error
-        if (alreadyBooked.isPresent())
+        if (slotAvailabilityRecord.isBooked())
             throw new GameszoneException("BookingService.SLOT_ALREADY_BOOKED");
 
         // if not then book slot
@@ -62,6 +65,31 @@ public class BookingServiceImpl implements BookingService {
         booking.setUser(user.orElseThrow(() -> new GameszoneException("UserService.USER_NOT_FOUND")));
 
         return bookingRepository.save(booking).tDto();
+    }
+
+    @Override
+    public SlotAvailabilityRecord fetchSlotAvailability(Integer slotId, Integer gameId, LocalDate forDate)
+            throws GameszoneException {
+
+        Optional<Booking> isSlotBooked = bookingRepository
+                .findByBookingDateAndGameIdAndSlotId(forDate, gameId, slotId);
+
+        SlotDTO slot = new SlotDTO();
+
+        Optional<Slot> optionalSlot = slotRepository.findByGameIdAndSlotId(gameId, slotId);
+
+        if (optionalSlot.isPresent()) {
+            slot.setAll(optionalSlot.get().toDto());
+        } else {
+            throw new GameszoneException("BookingService.SLOT_NOT_FOUND_GAMEID_SLOTID");
+
+        }
+
+        // availability
+        SlotAvailabilityRecord slotAvailabilityRecord = new SlotAvailabilityRecord(slot, isSlotBooked.isPresent(),
+                forDate);
+
+        return slotAvailabilityRecord;
     }
 
 }
