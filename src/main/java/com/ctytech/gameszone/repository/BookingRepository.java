3952f8ffa949c7b28bc261263b9aba58e3bdeac0
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-11 10:52:59 +0530

    added new query to find booking by bookingdate, gameId, slotId

--- src/main/java/com/ctytech/gameszone/repository/BookingRepository.java
+++ src/main/java/com/ctytech/gameszone/repository/BookingRepository.java
@@ -1,5 +1,9 @@
 package com.ctytech.gameszone.repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +12,6 @@ import com.ctytech.gameszone.entity.Booking;
 @Repository
 public interface BookingRepository extends CrudRepository<Booking, Integer> {
 
+    @Query(value = "SELECT * FROM booking b WHERE b.booking_date= ?1 AND b.game_id = ?2 AND b.slot_id = ?3", nativeQuery = true)
+    Optional<Booking> findByBookingDateAndGameIdAndSlotId(LocalDate bookingDate, Integer gameId, Integer slotId);
 }
