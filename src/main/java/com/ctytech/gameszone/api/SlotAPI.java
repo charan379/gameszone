79fc92978d38003c88c8648c5c67c2238813b03a
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-24 23:58:15 +0530

    slot page

--- src/main/java/com/ctytech/gameszone/api/SlotAPI.java
+++ src/main/java/com/ctytech/gameszone/api/SlotAPI.java
@@ -1,23 +1,28 @@
 package com.ctytech.gameszone.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ctytech.gameszone.api.responses.GenericResponse;
 import com.ctytech.gameszone.dto.SlotDTO;
 import com.ctytech.gameszone.exception.GameszoneException;
 import com.ctytech.gameszone.service.SlotService;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Pattern;
 
 @RestController
@@ -54,12 +59,36 @@ public class SlotAPI {
 
     @DeleteMapping(value = "/slot/{slotId}")
     @PreAuthorize(value = "hasRole('ADMIN')")
-    public ResponseEntity<String> deleteSlot(
+    public ResponseEntity<GenericResponse> deleteSlot(
             @PathVariable(name = "slotId") @Pattern(regexp = "^[0-9]*$", message = "{slot.slotId.invalid}") String slotId)
             throws GameszoneException {
 
         slotService.deleteSlot(Integer.parseInt(slotId));
 
-        return new ResponseEntity<>("Successfully Deleted", HttpStatus.OK);
+        GenericResponse genericResponse = new GenericResponse("Successfully Deleted");
+
+        return new ResponseEntity<>(genericResponse, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{gameId}/slots/search")
+    public ResponseEntity<Page<SlotDTO>> searchGameSlots(
+            @PathVariable(name = "gameId") @Pattern(regexp = "^[0-9]*$", message = "{game.gameId.invalid}") String gameId,
+            @RequestParam(name = "name", defaultValue = "") String nameQuery,
+            @RequestParam(name = "location", defaultValue = "") String locationQuery,
+            @RequestParam(name = "pageNo", defaultValue = "0") @Pattern(regexp = "^[0-9]*$", message = "{page.pageno.invalid}") String pageNo,
+            @RequestParam(name = "limit", defaultValue = "10") @Pattern(regexp = "^[0-9]*$", message = "{page.limit.invalid}") @Min(value = 1, message = "{page.limit.min}") String limit,
+            @RequestParam(name = "sort", defaultValue = "slotId.asc") @Pattern(regexp = "^(?:slotId|slotName|startTime|endTime|location)\\.(?:asc|desc)$", message = "{page.sort.invalid}") String sort)
+            throws GameszoneException {
+
+        Page<SlotDTO> slotsPage = slotService.searchSlots(
+                Integer.parseInt(gameId),
+                nameQuery,
+                locationQuery,
+                Integer.parseInt(pageNo),
+                Integer.parseInt(limit),
+                sort);
+
+        return new ResponseEntity<>(slotsPage, HttpStatus.OK);
+
     }
 }
--- src/main/java/com/ctytech/gameszone/repository/SlotRepository.java
+++ src/main/java/com/ctytech/gameszone/repository/SlotRepository.java
@@ -3,6 +3,8 @@ package com.ctytech.gameszone.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -18,4 +20,11 @@ public interface SlotRepository extends CrudRepository<Slot, Integer> {
 
     @Query(value = "SELECT * FROM slots s WHERE s.game_id = :gameId AND s.slot_id = :slotId", nativeQuery = true)
     Optional<Slot> findByGameIdAndSlotId(@Param("gameId") Integer gameId, @Param("slotId") Integer slotId);
+
+    @Query(value = "SELECT s FROM Slot s WHERE s.gameId = :gameId AND s.slotName LIKE %:nameQuery% AND s.location LIKE %:locationQuery%", countQuery = "SELECT COUNT(s) FROM Slot s WHERE s.gameId = :gameId AND s.slotName LIKE %:nameQuery% AND s.location LIKE %:locationQuery%")
+    Page<Slot> findBySlotQuery(
+            @Param("gameId") Integer gameId,
+            @Param("nameQuery") String nameQuery,
+            @Param("locationQuery") String locationQuery,
+            Pageable pageable);
 }
