b25a910d5ed47acf7ddc174a1cd2f9fe4421d26a
charan379 <37008444+charan379@users.noreply.github.com>
2023-12-02 18:54:50 +0530

    minor tweaks

--- src/main/java/com/ctytech/gameszone/GameszoneApplication.java
+++ src/main/java/com/ctytech/gameszone/GameszoneApplication.java
@@ -20,18 +20,23 @@ import jakarta.annotation.PostConstruct;
 @PropertySource(value = { "classpath:messages.properties" })
 public class GameszoneApplication {
 
+	// read properties from application.properties file -> app.config.time-zone
 	@Value("${app.config.time-zone:#{null}}")
 	private Optional<String> TIME_ZONE;
 
+	// logger
 	Logger logger = LoggerFactory.getLogger(GameszoneApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GameszoneApplication.class, args);
 	}
 
+	// This method is called when the application is started.
 	@PostConstruct
 	public void init() {
 
+		// set time zone for the application based on the application configuration file
+		// (applicatain.properties - app.config.time-zone)
 		if (TIME_ZONE.isPresent() && ZoneId.getAvailableZoneIds().contains(TIME_ZONE.get())) {
 			logger.info("Setting application timezone to " + TIME_ZONE.get());
 			TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of(TIME_ZONE.get())));
